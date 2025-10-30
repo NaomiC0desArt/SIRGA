3ad31c44058b666c5a02346ad73dc5cda8d36a33
Naomi Meran Padilla <20231514@itla.edu.do>
2025-10-29 22:42:29 -0400

    Benni can do a migration

---
+++ SIRGA.Persistence/DbContext/ApplicationDbContextFactory.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace SIRGA.Persistence.DbContext
+{
+    class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            
+            var basePath = Directory.GetCurrentDirectory();
+
+            
+            var pathToJson = Path.Combine(basePath, "../SIRGA.API/appsettings.json");
+            if (!File.Exists(pathToJson))
+            {
+                pathToJson = Path.Combine(basePath, "appsettings.json");
+            }
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile(pathToJson, optional: false)
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
