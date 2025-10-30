9b1cb1a18a6878b7dd29d6729d8d9fe984283861
Naomi Meran Padilla <20231514@itla.edu.do>
2025-10-29 23:27:06 -0400

    migracion agegada

---
+++ SIRGA.Persistence/Migrations/20251030032552_Agregar_Nuevos_Campo.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SIRGA.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Agregar_Nuevos_Campo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Specialty",
+                table: "Profesores",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Matricula",
+                table: "Estudiantes",
+                type: "nvarchar(4)",
+                maxLength: 4,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Estudiantes_Matricula",
+                table: "Estudiantes",
+                column: "Matricula",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Estudiantes_Matricula",
+                table: "Estudiantes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Specialty",
+                table: "Profesores",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Matricula",
+                table: "Estudiantes",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(4)",
+                oldMaxLength: 4);
+        }
+    }
+}
